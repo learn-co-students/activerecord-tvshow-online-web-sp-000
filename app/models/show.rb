3577756cb32586a90a84aa1f62729ca26f39799f
Diana Badas <diana.badas@gmail.com>
2019-03-13 11:56:48 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,29 +1,29 @@
 class Show < ActiveRecord::Base
-  def Show::highest_rating
+  def self.highest_rating
     Show.maximum(:rating)
   end
 
-  def Show::most_popular_show
+  def self.most_popular_show
     Show.where("rating = ?", Show.highest_rating)
   end
 
-  def Show::lowest_rating
+  def self.lowest_rating
     Show.minimun(:rating)
   end
 
-  def Show::least_popular_show
+  def self.least_popular_show
     Show.where("rating = ?", Show.lowest_rating)
   end
 
-  def Show::ratings_sum
+  def self.ratings_sum
     Show.sum(:rating)
   end
 
-  def Show::popular_shows
+  def self.popular_shows
     Show.where("rating > ?", 5)
   end
 
-  def Show::shows_by_alphabetical_order
+  def self.shows_by_alphabetical_order
     Show.order(:name)
   end
 end
