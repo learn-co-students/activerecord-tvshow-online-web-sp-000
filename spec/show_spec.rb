de42493d95252256a8b3b8f162bfa1cbd6ce160a
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-08-01 02:35:07 +0000

    Done.

--- spec/show_spec.rb
+++ spec/show_spec.rb
@@ -6,6 +6,7 @@ describe Show do
     Show.destroy_all
   end
 
+
   before(:each) do
     @rick_and_morty = Show.create(:name => "Rick and Morty", :day => "Thursday", :network => "Adult Swim", :rating => 10)
     @law_and_order = Show.create(:name => "Law & Order", :day => "Monday", :network => "NBC", :rating => 7)
