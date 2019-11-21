dfc39369decee8d26c97fa04e7036ab9dc4c1e28
Brian Pompey <brianthescholar@gmail.com>
2019-11-21 18:19:21 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 3) do
 
   create_table "shows", force: :cascade do |t|
     t.string "name"
     t.string "network"
     t.string "day"
-    t.integer "integer"
+    t.integer "rating"
     t.string "season"
   end
 
