b67da2d9d5292389a62dfa01f3ea289758fe7d1d
Chad Lengefeld <chadlengefeld@gmail.com>
2020-03-13 22:31:59 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,8 +1,34 @@
 require 'pry'
 class Show < ActiveRecord::Base
 
-  def highest_rating
+  def self.highest_rating
     Show.maximum(:rating)
   end
+
+  def self.most_popular_show
+  Show.where(rating: self.highest_rating).first
+    # (highest_rating)
+  end
+
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end
+
+  def self.least_popular_show
+      Show.where(rating: self.lowest_rating).first
+  end
+
+  def self.ratings_sum
+    Show.sum(:rating)
+  end
+
+  def self.popular_shows
+    Show.where('rating > 5')
+  end
+
+  def self.shows_by_alphabetical_order
+    Show.order(name: :asc)
+  end
+
 
 end
