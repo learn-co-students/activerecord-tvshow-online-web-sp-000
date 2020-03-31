0bdf15221e3ba9fb83de464a994eb8865babb1b7
Ronda D Cowen <cowenhome5@gmail.com>
2020-03-31 20:28:20 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,14 +5,16 @@ class Show < ActiveRecord::Base
 
   end
   def self.most_popular_show
-    # binding.pry
-    self.where('rating >= ?', 10)
+
+    self.where('rating >= ?', 10).first
 
   end
   def self.lowest_rating
     self.minimum(:rating)
+  
   end
   def self.least_popular_show
+    self.where('rating <= ?', 2).first
   end
   def self.ratings_sum
     self.sum(:rating)
