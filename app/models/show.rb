168292672a36957d1da46f1512b04e7ea65dfb64
Mark A Guza <guzamaguza@gmail.com>
2020-04-07 05:50:59 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,24 @@
 class Show < ActiveRecord::Base
+
+  def Show::highest_rating
+  end
+
+  def Show::most_popular_show
+  end
+
+  def Show::lowest_rating
+  end
+
+  def Show::least_popular_show
+  end
+
+  def ratings_sum
+  end
+
+  def popular_shows
+  end
+
+  def shows_by_alphabetical_order
+  end
+
 end
