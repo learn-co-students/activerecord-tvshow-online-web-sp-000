e6540269d7ba0aa936c05d5e712df2bb8f4750a5
Mark A Guza <guzamaguza@gmail.com>
2020-04-16 05:26:22 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,24 +1,32 @@
 class Show < ActiveRecord::Base
+  attr_accessor :name, :network, :day, :rating, :season
 
   def Show::highest_rating
+    Show.maximum(:highest_rating).value
   end
 
   def Show::most_popular_show
+    Show.maximum(:rating)
   end
 
   def Show::lowest_rating
+    Show.minimum(:rating).value
   end
 
   def Show::least_popular_show
+    Show.minimum(:rating)
   end
 
   def ratings_sum
+    Show.sum(:rating)
   end
 
   def popular_shows
+    Show.where(":rating > ?" , 5)
   end
 
   def shows_by_alphabetical_order
+    Show.order(:name)
   end
 
 end
