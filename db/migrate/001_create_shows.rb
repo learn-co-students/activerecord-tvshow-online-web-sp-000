7a01725e48531e24f082b774ad4540c15ed73f81
jkellyphilly <joel.kelly.philly@gmail.com>
2020-04-20 22:01:10 -0400

    Done.

---
+++ db/migrate/001_create_shows.rb
@@ -0,0 +1,12 @@
+# Class to perform migration to create a shows table
+
+class CreateShows < ActiveRecord::Migration[5.2]
+  def change
+    create_table :shows do |t|
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating
+    end
+  end
+end
