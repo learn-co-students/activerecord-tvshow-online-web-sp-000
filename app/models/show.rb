a3b56acc84eb5259ef2c42a725b70ed8aa25556f
Cody Alvarez <cody_alvarez19@outlook.com>
2020-07-09 03:27:45 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,27 @@
 class Show < ActiveRecord::Base
+  def self.highest_rating
+    self.maximum(:rating)
+  end
+
+  def self.most_popular_show
+    self.highest_rating(:name)
+  end
+
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
+
+  def self.least_popular_show
+  end
+
+  def self.ratings_sum
+    self.sum(:rating)
+  end
+
+  def self.popular_shows
+  end
+
+  def self.shows_by_alphabetical_order
+  end
+
 end
--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -8,25 +8,4 @@ class CreateShows < ActiveRecord::Migration[5.2]
       t.string :season
     end
   end
-
-  def Show::highest_rating
-  end
-
-  def Show::most_popular_show
-  end
-
-  def Show::lowest_rating
-  end
-
-  def Show::least_popular_show
-  end
-
-  def Show::ratings_sum
-  end
-
-  def Show::popular_shows
-  end
-
-  def Show::shows_by_alphabetical_order
-  end
 end
