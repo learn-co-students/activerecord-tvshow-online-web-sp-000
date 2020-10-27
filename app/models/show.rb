4c37a8de6f3f0934f4c33d1a4817ee70f46c6cc1
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2020-10-27 10:52:25 -0500

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,16 @@
+require 'pry'
 class Show < ActiveRecord::Base
+    def self.highest_rating # returns the highest value in the ratings column
+        self.maximum('rating')
+    end
+
+    def self.most_popular_show # returns the show with the highest rating
+        self.find_by! rating: '10'
+        #binding.pry
+    end
+
+    def self.lowest_rating # returns the lowest value in the ratings column
+        self.minimum('rating')
+    end
+
 end
