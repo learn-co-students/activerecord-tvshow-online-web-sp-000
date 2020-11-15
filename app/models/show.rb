c849a4c5f790e771279cc8c65cb26e5093676363
Ryan Burns <ryankburns@gmail.com>
2020-11-14 16:26:17 -0800

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,11 @@
+class Show < ActiveRecord::Base
+
+    def Show.highest_rating
+        Show.maximum(:rating)
+    end
+
+    def Show.most_popular_show
+        Show.where("rating = ?", highest_rating)
+    end
+
+end
