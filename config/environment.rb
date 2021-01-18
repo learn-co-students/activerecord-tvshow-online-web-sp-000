fa8edd300dfd4ed51cde8565a84be96d7f548d1c
Chris Houghton <chrishoughton2434@gmail.com>
2021-01-18 17:19:58 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Bundler.require
 
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
 Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
-
+require_relative "../app/controller.rb"
 DB = ActiveRecord::Base.establish_connection({
   adapter: 'sqlite3',
   database: 'db/tvshows.db'
@@ -15,3 +15,4 @@ DB = ActiveRecord::Base.establish_connection({
 if ENV["ACTIVE_RECORD_ENV"] == "test"
   ActiveRecord::Migration.verbose = false
 end
+
---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+Show.create(name: "Wargames", network: 'NBC', day: 'Wednesday', rating: 3)
+
+Show.create(name: "The Sting", network: 'Comedy Central', day: 'Thursday', rating: 5)
+
+Show.create(name: "Nebraska", network: 'TNT', day: 'Friday', rating: 6)
+
+Show.create(name: "Muppets Most Wanted", network: 'HBO', day: 'Saturday', rating: 7)
+
+Show.create(name: "loosy Goosy", network: 'STARZ', day: 'Sunday', rating: 9)
+
